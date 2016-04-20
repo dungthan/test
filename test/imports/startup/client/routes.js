ee3c792b0c71299ab75719b1df96dba1bcf69fa2
DungThan <dungthan@abcs-Mac-mini.local>
2016-04-20 18:22:18 +0700

    account

--- test/imports/startup/client/routes.js
+++ test/imports/startup/client/routes.js
@@ -1,12 +1,33 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+import { AccountsTemplates } from 'meteor/useraccounts:core';
 
 import '../../ui/layouts/app-body.js';
 import '../../ui/pages/root-redirector.js';
+
+import '../../ui/accounts/accounts-templates.js'
 
 FlowRouter.route('/', {
 	name: 'App.home',
 	action() {
 		BlazeLayout.render('App_body', { main: 'app_rootRedirector' });
 	}
+});
+
+AccountsTemplates.configureRoute('signIn', {
+	name: 'signin',
+	path: '/signin',
+});
+
+AccountsTemplates.configureRoute('signUp', {
+	name: 'join',
+	path: '/join',
+});
+
+AccountsTemplates.configureRoute('forgotPwd');
+
+AccountsTemplates.configureRoute('resetPwd', {
+	name: 'resetPwd',
+	path: '/reset-password',
 })
+
